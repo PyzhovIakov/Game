ecc6806fab36a5eb7b907111b139c587237c7ab8
Pyzhov <pyzovakov@mail.ru>
2021-11-30 17:24:52 +0300

    Organization of ship movement (4 commits)

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from ship import Ship
 from settings import Settings
+
 """Класс для управления ресурсами и поведением игры."""
 
 
@@ -20,16 +21,44 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        # При каждом проходе цикла перерисовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 
 # Создание экземпляра и запуск игры.
